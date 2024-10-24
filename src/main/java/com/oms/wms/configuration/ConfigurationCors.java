64d23005aa7675021b621e0dcf517dcbe7f5de80
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2024-10-24 14:23:44 -0300

    refactor: update cors methods

--- src/main/java/com/oms/wms/configuration/ConfigurationCors.java
+++ src/main/java/com/oms/wms/configuration/ConfigurationCors.java
@@ -1,5 +1,6 @@
 package com.oms.wms.configuration;
 
+import lombok.NonNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,10 +13,13 @@ public class ConfigurationCors {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("POST", "GET", "PUT", "DELETE");
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("POST", "GET", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
             }
         };
     }
